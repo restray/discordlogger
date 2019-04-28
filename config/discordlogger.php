dd2f048303baa60dd9762001a42e113d1a04283f
Restray <contact@restray.org>
2019-04-28 11:13:05 +0200

    Fix bug

--- config/discordlogger.php
+++ config/discordlogger.php
@@ -5,7 +5,7 @@ return [
     * You can choose if all the messages are published on the same channel or if
     * messages are splits.
     ****************************************************************************/
-    'webhook' => 'https://discordapp.com/api/webhooks/513435303219036191/LYDRLw1veti3NVbqQwid5ZKfgdCKpv9qr63-XU3sY52bneDo5KZN4-3HB4a6N5KlLZCg',
+    'webhook' => '',
 
     'webhooks' => [
         'info' => '',
